1a59b7a4279667fe3c8ac2a0dc2f15fcbd67171a
Nic Last <niclast7611@gmail.com>
2022-07-05 17:19:33 -0700

    app has initial styling need to work on functionality and a bit more styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Macro from './components/Macro.js';
+import Result from './components/Result.js';
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <Macro />
+      <Result />
+      </>
+    )
+  }
 }
 
-export default App;
